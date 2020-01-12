cdf9e343503256e1c92bf2690c51ddbffa9bc373
Lauri Kemppainen <laukempp@gmail.com>
2020-01-12 03:38:57 +0200

    contact routes & also the whole backend kinda finalized. React front created

--- routes/contacts.js
+++ routes/contacts.js
@@ -10,38 +10,117 @@ const Contact = require("../Models/Contact");
 // @desc        Get all users contacts
 // @access      Private
 //Try & catch because we're dealing with Mongoose
-router.get("/", auth, (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
-      //Contact -model has a "user" -field, which has objectID. Since we're using the auth -middleware we have access to the req.user object. -1 in sort makes the most recent contacts appear first.
-      const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
-      res.json(contacts);
-
-      
+    //Contact -model has a "user" -field, which has objectID. Since we're using the auth -middleware we have access to the req.user object. -1 in sort makes the most recent contacts appear first.
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
   } catch (err) {
-console.error(err.message)
-res.status(500).send('Server error')      
+    console.error(err.message);
+    res.status(500).send("Server error");
   }
 });
 
 // @route       POST api/contacts
 // @desc        Add new contact
 // @access      Private
-router.post("/", (req, res) => {
-  res.send("Add a contact");
-});
+// To use several middlewares in one route (auth, validator here) make a bracket
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("name", "Name is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //Since we're using validator -> the same error checking as in users -routes.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 // @route       PUT api/contacts/:id || id on placeholder sille kontaktille, mitä halutaan muokata
 // @desc        Update contact
 // @access      Private
-router.put("/:id", (req, res) => {
-  res.send("Update contact");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build contact object
+  const contactFields = {};
+  // If there is a name -> let's add it to that contactFields
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    // req.params.id avulla päästään käsiksi "/:id" polkuun
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 // @route       PUT api/contacts/:id
 // @desc        Update contact
 // @access      Private
-router.delete("/:id", (req, res) => {
-  res.send("Delete contact");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    // req.params.id avulla päästään käsiksi "/:id" polkuun
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: "Contact removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
